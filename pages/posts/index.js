bcac6e900678e8d44fcc81b7ae6710160d0ce28e
Akshay <akshaymangutkar951@gmail.com>
2025-04-14 15:49:15 +0530

    added all post page with dummy data

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,38 @@
-function AllPostPage() {}
+import AllPosts from "../../components/posts/all-post";
+
+const DUMMY_POST = [
+  {
+    title: "getting-started-with 1",
+    image: "getting-started-nextjs.png",
+    excerpt: "hello how r u hope u r well and i m",
+    date: "2025-02-12",
+    slug: "getting-started-with",
+  },
+  {
+    title: "getting-started-with 2",
+    image: "getting-started-nextjs.png",
+    excerpt: "hello how r u hope u r well and i m",
+    date: "2025-02-12",
+    slug: "getting-started-with",
+  },
+  {
+    title: "getting-started-with 3",
+    image: "getting-started-nextjs.png",
+    excerpt: "hello how r u hope u r well and i m",
+    date: "2025-02-12",
+    slug: "getting-started-with",
+  },
+  {
+    title: "getting-started-with 4",
+    image: "getting-started-nextjs.png",
+    excerpt: "hello how r u hope u r well and i m",
+    date: "2025-02-12",
+    slug: "getting-started-with",
+  },
+];
+
+function AllPostPage() {
+  return <AllPosts posts={DUMMY_POST} />;
+}
 
 export default AllPostPage;
